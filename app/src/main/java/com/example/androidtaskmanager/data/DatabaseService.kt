a017678e3ebb9af4bb89fd96aa8ab2527e2d0d3e
sanyament <billion1cash1337@gmail.com>
2026-02-20 22:09:44 +0700

    add the minimal architechture

---
+++ app/src/main/java/com/example/androidtaskmanager/data/DatabaseService.kt
@@ -0,0 +1,4 @@
+package com.example.androidtaskmanager.data
+
+class DatabaseService {
+}
---
+++ app/src/main/java/com/example/androidtaskmanager/models/User.kt
@@ -0,0 +1,7 @@
+package com.example.androidtaskmanager.models
+
+class User {
+    companion object{
+        var age: Int = 3;
+    }
+}
